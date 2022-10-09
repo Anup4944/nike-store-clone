a90a14173631f323d7c5b685acd614e4129153fc
Anup <anuppoudel60@gmail.com>
2022-10-09 18:46:16 +1100

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+    screens: {
+      xl: { max: "1200px" },
+      lg: { max: "991px" },
+      md: { max: "767px" },
+      sm: { max: "550px" },
+      xsm: { max: "357px" },
+    },
+  },
+  plugins: [],
+};
