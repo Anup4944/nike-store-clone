b1f18885467ac9eaf4fc8f4377c6912376ff3729
Anup <anuppoudel60@gmail.com>
2022-10-10 13:33:09 +1100

    top storeis, navbar and footer added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from "react";
-import { FlexContent, Hero, Sales } from "./components";
+import {
+  FlexContent,
+  Hero,
+  Sales,
+  Stories,
+  Footer,
+  Navbar,
+} from "./components";
 import {
   heroApi,
   popularsales,
   toprateslaes,
   highlight,
   sneaker,
+  story,
+  footerAPI,
 } from "./data/data";
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <main className="flex flex-col gap-16 relative">
         <Hero heroApi={heroApi} />
         <Sales endPoint={popularsales} ifExists />
         <FlexContent endPoint={sneaker} ifExists />
         <Sales endPoint={toprateslaes} />
         <FlexContent endPoint={highlight} />
+        <Stories story={story} />
       </main>
+      <Footer footerAPI={footerAPI} />
     </>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 export { default as Hero } from "./Hero.jsx";
 export { default as Sales } from "./Sales.jsx";
 export { default as FlexContent } from "./FlexContent.jsx";
+export { default as Stories } from "./Stories.jsx";
+export { default as Footer } from "./Footer.jsx";
+export { default as Navbar } from "./Navbar.jsx";
